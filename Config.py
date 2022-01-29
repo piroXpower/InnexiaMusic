12eefbc08c5d08b8cccfdb8bcb4592f79667fbdd
JorDenXD <98655130+JorDenXD@users.noreply.github.com>
2022-01-29 21:56:32 +0530

    Create Config.py

---
+++ Config.py
@@ -0,0 +1,49 @@
+import os
+from dotenv import load_dotenv
+from os import getenv
+from pyrogram import Client, filters
+from pytgcalls import PyTgCalls
+
+
+if os.path.exists(".env"):
+    load_dotenv(".env")
+    
+load_dotenv()
+admins = {}
+API_ID = int(os.getenv("API_ID", "6"))
+API_HASH = os.getenv("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
+SESSION_NAME = os.getenv("SESSION")
+BOT_TOKEN = getenv("BOT_TOKEN", "")
+BOT_NAME = getenv("BOT_NAME", "HELL MUSIC")
+HNDLR = os.getenv("HNDLR", "!")
+GROUP_MODE = os.getenv("GROUP_MODE", "True")
+OWNER_NAME = getenv("OWNER_NAME", "ABHISHEK_XDD")
+ALIVE_NAME = getenv("ALIVE_NAME", "")
+BOT_USERNAME = getenv("BOT_USERNAME", "")
+ASSISTANT_NAME = getenv("ASSISTANT_NAME", "")
+GROUP_SUPPORT = getenv("GROUP_SUPPORT", "SNEHABHI_SERVER")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "VEXERA_UPDATES")
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5079644547").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5079644547").split()))
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/levina-lab/video-stream")
+IMG_1 = getenv("IMG_1", "https://telegra.ph/file/d6f92c979ad96b2031cba.png")
+IMG_2 = getenv("IMG_2", "https://telegra.ph/file/6213d2673486beca02967.png")
+IMG_3 = getenv("IMG_3", "https://telegra.ph/file/f02efde766160d3ff52d6.png")
+IMG_4 = getenv("IMG_4", "https://telegra.ph/file/be5f551acb116292d15ec.png")
+IMG_5 = getenv("IMG_5", "https://telegra.ph/file/c3401a572375b569138c3.png")
+IMG_6 = getenv("IMG_6", "https://telegra.ph/file/c3401a572375b569138c3.png")
+
+contact_filter = filters.create(
+    lambda _, __, message:
+    (message.from_user and message.from_user.is_contact) or message.outgoing
+)
+
+
+if GROUP_MODE == ("True" or "true"):
+    grp = True
+else:
+    grp = False
+
+GRPPLAY = grp
+bot = Client(SESSION, API_ID, API_HASH, plugins=dict(root="HellMusic"))
+semx = PyTgCalls(bot)
