35a24ee27a4877e9d1759405bbcb5e641d7640f8
eVEXERA <96389440+eVEXERA@users.noreply.github.com>
2022-01-31 09:20:47 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,47 +1,33 @@
+## What's up Kangers
+
 import os
-from dotenv import load_dotenv
 from os import getenv
-from pyrogram import Client, filters
-from pytgcalls import PyTgCalls
+from dotenv import load_dotenv
 
+if os.path.exists("local.env"):
+    load_dotenv("local.env")
 
-if os.path.exists(".env"):
-    load_dotenv(".env")
-    
 load_dotenv()
 admins = {}
-API_ID = int(getenv("API_ID", "12281520"))
-API_HASH = getenv("API_HASH", "60fb92420c4797fb1e1af54a16edaf55")
 SESSION_NAME = getenv("SESSION_NAME", "session")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-BOT_NAME = getenv("BOT_NAME", "HELL MUSIC")
-HNDLR = os.getenv("HNDLR", "!")
-GROUP_MODE = os.getenv("GROUP_MODE", "True")
-OWNER_NAME = getenv("OWNER_NAME", "ABHISHEK_XDD")
+BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_NAME = getenv("BOT_NAME", "")
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+OWNER_NAME = getenv("OWNER_NAME", "SAMMYXD")
 ALIVE_NAME = getenv("ALIVE_NAME", "")
 BOT_USERNAME = getenv("BOT_USERNAME", "")
 ASSISTANT_NAME = getenv("ASSISTANT_NAME", "")
-GROUP_SUPPORT = getenv("GROUP_SUPPORT", "SNEHABHI_SERVER")
-UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "VEXERA_UPDATES")
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5079644547").split()))
+GROUP_SUPPORT = getenv("GROUP_SUPPORT", "PmPermit")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "BotDuniya")
+SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
+COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
+ALIVE_IMG = getenv("ALIVE_IMG", "https://telegra.ph/file/c83b000f004f01897fe18.png")
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "60"))
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/levina-lab/video-stream")
 IMG_1 = getenv("IMG_1", "https://telegra.ph/file/d6f92c979ad96b2031cba.png")
 IMG_2 = getenv("IMG_2", "https://telegra.ph/file/6213d2673486beca02967.png")
 IMG_3 = getenv("IMG_3", "https://telegra.ph/file/f02efde766160d3ff52d6.png")
 IMG_4 = getenv("IMG_4", "https://telegra.ph/file/be5f551acb116292d15ec.png")
-IMG_5 = getenv("IMG_5", "https://telegra.ph/file/c3401a572375b569138c3.png")
-IMG_6 = getenv("IMG_6", "https://telegra.ph/file/c3401a572375b569138c3.png")
-
-contact_filter = filters.create(
-    lambda _, __, message:
-    (message.from_user and message.from_user.is_contact) or message.outgoing
-)
-
-
-if GROUP_MODE == ("True" or "true"):
-    grp = True
-else:
-    grp = False
-
-GRPPLAY = grp
 
 
