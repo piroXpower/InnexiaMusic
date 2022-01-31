dafcdd703804765b9994d560691454c8ac612f1a
eVEXERA <96389440+eVEXERA@users.noreply.github.com>
2022-01-31 09:21:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import asyncio
 from pytgcalls import idle
-from Process.main import call_py, bot
+from ImageFont.main import call_py, bot
 
 async def start_bot():
     print("[INFO]: STARTING BOT CLIENT")
