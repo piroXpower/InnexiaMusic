cff342d8c6b361add46c4e8fd9b42f9731e3967c
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-06-01 11:53:55 +0530

    Now Ok

--- Music/Client/tgcalls.py
+++ Music/Client/tgcalls.py
@@ -1,1 +1,10 @@
+from pyrogram import Client
 from pyrogram_bot.abhi import Bot, abhi, calls
+from pytgcalls import PyTgCalls, idle
+
+call_py = PyTgCalls(abhi, overload_quiet_mode=True)
+
+with Client("Pyrogram Bot", API_ID, API_HASH, bot_token=BOT_TOKEN) as app:
+    me_bot = app.get_me()
+with abbi as app:
+    me_abhi = app.get_me()
