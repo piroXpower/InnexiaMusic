6e8de7df116894b28eff88c4caa546f03103c1bc
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-06-01 18:03:09 +0530

    Update callbacks.py

--- Music/plugins/callbacks.py
+++ Music/plugins/callbacks.py
@@ -1,7 +1,7 @@
 import yt_dlp
 from Music.Client.queues import QUEUE
 from pyrogram import Client, filters
-from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup)
+from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
 from Music.plugins.keyboard import play_markup
 
 @Client.on_callback_query(filters.regex("close"))
