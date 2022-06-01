2c20c813c619a0cc3a217e1191781c56968f4e81
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-06-01 12:31:38 +0530

    some Measure Changes Fixing Join Issue

--- Music/__main__.py
+++ Music/__main__.py
@@ -7,13 +7,29 @@ from pyrogram_bot.abhi import Bot, abhi
 async def start_bot():
     print("[INFO]: STARTING BOT CLIENT")
     await Bot.start()
-    await Bot.send_message(LOGGER, " <b> InnexiaMusic Bot Successfully Started </b> ") 
+    await Bot.send_message(
+                           LOG_CHAT,
+                           "<b>InnexiaMusic Bot Successfully Started!</b>"
+    ), 
+
     print("[INFO]: STARTING PYTGCALLS CLIENT")
+
     await calls_py.start()
-    await abhi.send_message(LOGGER, " <b> Innexia Music Assistant Successfully Started </b> ") 
+    await abhi.send_message(
+                            LOGGER,
+                            "<b>Innexia Music Assistant Successfully Started!</b>"
+    ), 
+    await abhi.join_chat("RexomaSupport"), 
+    await abhi.join_chat("RexomaUpdate") 
     await idle()
+
     print("[INFO]: STOPPING BOT & USERBOT")
-    await Bot.send_message(LOGGER, "**Error Occurred Bot And Assistant Being Stopped**") 
+
+    await Bot.send_message(
+                           LOGGER,
+                           "<b>Error Occurred Bot And Assistant Being Stopped</b>"
+    ), 
+
     await Bot.stop()
 
 
