ebd51861fa484c6384769999e4dea9d24a4149e6
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-06-01 17:55:40 +0530

    Update tgcalls.py

--- Music/Client/tgcalls.py
+++ Music/Client/tgcalls.py
@@ -1,28 +1,28 @@
-import os
-from os import getenv
 from pyrogram import Client
+from Music import API_ID, API_HASH, BOT_TOKEN, SESSION_NAME
 from pytgcalls import PyTgCalls, idle
-from config import API_ID, API_HASH, SESSION_NAME, BOT_TOKEN
 
-Bot = Client(
-    "Bot",
+bot = Client(
+    "Music",
+    API_ID,
+    API_HASH,
     bot_token=BOT_TOKEN,
-    api_id=API_ID,
-    api_hash=API_HASH
-)
+    plugins=dict(root="Script.Plugin"),
+    )
 
 sex = Client(
-      api_id=API_ID, 
-      api_hash=API_HASH, 
-      session_name=SESSION_NAME
-) 
+    api_id=API_ID,
+    api_hash=API_HASH,
+    session_name=SESSION_NAME,
+    
+    )
+
 user = PyTgCalls(sex,
     cache_duration=100,
     overload_quiet_mode=True,)
 
-call_py = PyTgCalls(sex, overload_quiet_mode=True)
 
-with Client("Bot", API_ID, API_HASH, bot_token=BOT_TOKEN) as app:
-    me_bot = app.get_me()
+with Client("Music", API_ID, API_HASH, bot_token=BOT_TOKEN) as app:
+    indian = app.get_me()
 with sex as app:
     me_sex = app.get_me()
