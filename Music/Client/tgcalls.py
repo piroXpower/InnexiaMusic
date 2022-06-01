191a6b3ef366405653a307d9f22e92a1879cc373
ABHISHEK THAKUR <99659192+MrAbhiX@users.noreply.github.com>
2022-06-01 12:16:59 +0530

    Update tgcalls.py

--- Music/Client/tgcalls.py
+++ Music/Client/tgcalls.py
@@ -1,8 +1,8 @@
 from pyrogram import Client
-from pyrogram_bot.abhi import Bot, abhi, calls
+from pyrogram_bot.abhi import *
 from pytgcalls import PyTgCalls, idle
 
-call_py = PyTgCalls(abhi, overload_quiet_mode=True)
+call_py = PyTgCalls(abhi, cache_duration=100, overload_quiet_mode=True)
 
 with Client("Pyrogram Bot", API_ID, API_HASH, bot_token=BOT_TOKEN) as app:
     me_bot = app.get_me()
