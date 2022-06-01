32df32966997c8dc0683877bb05b8a04ddb22857
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-06-01 11:58:05 +0530

    Update __main__.py

--- Music/__main__.py
+++ Music/__main__.py
@@ -1,6 +1,7 @@
 import asyncio
 from pytgcalls import idle
 from Music import LOGGER
+from Client.tgcalls import call_py
 from pyrogram_bot.abhi import Bot, calls, abhi
 
 async def start_bot():
@@ -8,7 +9,7 @@ async def start_bot():
     await Bot.start()
     await Bot.send_message(LOGGER, " <b> InnexiaMusic Bot Successfully Started </b> ") 
     print("[INFO]: STARTING PYTGCALLS CLIENT")
-    await calls.start()
+    await calls_py.start()
     await abhi.send_message(LOGGER, " <b> Innexia Music Assistant Successfully Started </b> ") 
     await idle()
     print("[INFO]: STOPPING BOT & USERBOT")
