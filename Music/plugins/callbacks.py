01ba069cf55baa930eb7d49ddf8e0b4abcd72dfb
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-06-01 14:23:34 +0530

    Update callbacks.py

--- Music/plugins/callbacks.py
+++ Music/plugins/callbacks.py
@@ -1,6 +1,6 @@
 import yt_dlp
-from Innexia.Client.queues import QUEUE
+from Music.Client.queues import QUEUE
 from pyrogram import Client, filters
 from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup)
-from RaiChu.inline import menu_markup, song_download_markup, stream_markup, audio_markup
+from Music.plugins.keyboard import play_markup
 
