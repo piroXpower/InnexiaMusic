32be56d291e786c1d43f8a36b679783fda65ab67
ABHISHEK THAKUR <99659192+MrAbhiX@users.noreply.github.com>
2022-06-01 11:26:33 +0530

    Update __main__.py

--- Music/__main__.py
+++ Music/__main__.py
@@ -1,19 +1,19 @@
 import asyncio
 from pytgcalls import idle
-from Music import call_py, bot, LOGGER
+from Music import LOGGER
 from pyrogram_bot.abhi import Bot, calls, abhi
 
 async def start_bot():
     print("[INFO]: STARTING BOT CLIENT")
-    await bot.start()
-    await bot.send_message(LOGGER, " <b> InnexiaMusic Bot Successfully Started </b> ") 
+    await Bot.start()
+    await Bot.send_message(LOGGER, " <b> InnexiaMusic Bot Successfully Started </b> ") 
     print("[INFO]: STARTING PYTGCALLS CLIENT")
-    await call_py.start()
-    await     .send_message(LOGGER, " <b> Innexia Music Assistant Successfully Started </b> ") 
+    await calls.start()
+    await abhi.send_message(LOGGER, " <b> Innexia Music Assistant Successfully Started </b> ") 
     await idle()
     print("[INFO]: STOPPING BOT & USERBOT")
-    await bot.send_message(LOGGER, "**Error Occurred Bot And Assistant Being Stopped**") 
-    await bot.stop()
+    await Bot.send_message(LOGGER, "**Error Occurred Bot And Assistant Being Stopped**") 
+    await Bot.stop()
 
 
 loop = asyncio.get_event_loop()
