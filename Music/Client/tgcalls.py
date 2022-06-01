ef88fab0ba89fd6ce6c422d7c28ba60860b2e2bf
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-06-01 16:07:59 +0530

    Update tgcalls.py

--- Music/Client/tgcalls.py
+++ Music/Client/tgcalls.py
@@ -1,6 +1,27 @@
+import os
+from os import getenv
 from pyrogram import Client
-from pyrogram_bot.abhi import *
 from pytgcalls import PyTgCalls, idle
+from config import API_ID, API_HASH, SESSION_NAME, BOT_TOKEN
+
+Bot = Client(
+    "Pyrogram Bot",
+    bot_token=BOT_TOKEN,
+    api_id=API_ID,
+    api_hash=API_HASH
+)
+
+
+abhi = Client(
+    api_id=API_ID, 
+    api_hash=API_HASH, 
+    session_name=SESSION_NAME
+    
+    )
+
+calls = PyTgCalls(abhi,
+    cache_duration=100,
+    overload_quiet_mode=True,)
 
 call_py = PyTgCalls(abhi, cache_duration=100, overload_quiet_mode=True)
 
