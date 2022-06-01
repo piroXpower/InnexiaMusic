a9995db237f27cd340a541e5a574b5584c126cb8
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-06-01 19:13:17 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import asyncio
 from pytgcalls import idle
 from Music import LOG_CHAT
-from Music.Client.tgcalls import Mikki, Bot, sex as abhi
+from Music.Client.tgcalls import Mikki, sex, Bot
 
 loop = asyncio.get_event_loop()
 
@@ -14,15 +14,20 @@ async def start_bot():
     ), 
 
     print("[INFO]: STARTING PYTGCALLS CLIENT")
-
-    await call_py.start()
-    await idle()
-    await abhi.send_message(
-                            LOGGER,
-                            "<b>Innexia Music Assistant Successfully Started!</b>"
-    ), 
-    await abhi.join_chat("RexomaSupport"), 
-    await abhi.join_chat("RexomaUpdate") 
+    await sex.start()
+    await sex.send_message(LOG_CHAT, "<b> Assistant Successfully Started </b>") 
+    await Mikki.start()
+    try:
+        await Mikki.stream_call(
+            LOG_CHAT, 
+            "http://docs.evostream.com/sample_content/assets/sintel1m720p.mp4"
+        )
+    except NoActiveGroupCall:
+        print("Please Turn On Voice Chat In LOG_CHAT") 
+        sys.exit()
+    await idle() 
+    await sex.join_chat("RexomaSupport"), 
+    await sex.join_chat("RexomaUpdate") 
     print("[INFO]: STOPPING BOT & USERBOT")    
     await Bot.stop()
 
