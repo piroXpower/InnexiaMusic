c83b67d444f0a2d3e7fe2483a932945e9b25c530
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-06-01 14:24:50 +0530

    Update callbacks.py

--- Music/plugins/callbacks.py
+++ Music/plugins/callbacks.py
@@ -4,3 +4,6 @@ from pyrogram import Client, filters
 from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup)
 from Music.plugins.keyboard import play_markup
 
+@Client.on_callback_query(filters.regex("close"))
+async def close(_, query: CallbackQuery):
+    await query.message.delete()
