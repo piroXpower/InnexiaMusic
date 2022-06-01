d6b7b918dbe37ec0263721f26867d6f71835e24b
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-06-01 17:51:02 +0530

    Update __main__.py

--- Music/__main__.py
+++ Music/__main__.py
@@ -14,7 +14,7 @@ async def start_bot():
     print("[INFO]: STARTING PYTGCALLS CLIENT")
 
     await call_py.start()
-    
+    await idle()
     await abhi.send_message(
                             LOGGER,
                             "<b>Innexia Music Assistant Successfully Started!</b>"
@@ -24,4 +24,4 @@ async def start_bot():
     print("[INFO]: STOPPING BOT & USERBOT")    
     await Bot.stop()
 
-await idle()
+
