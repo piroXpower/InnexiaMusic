2da924af5df08665dc48f8bf8024e2498eae69f1
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-06-01 14:15:01 +0530

    Update play.py

--- Music/plugins/keyboard/play.py
+++ Music/plugins/keyboard/play.py
@@ -1,1 +1,19 @@
+import random
+from pyrogram.types import InlineKeyboardButton
 
+def stream_markup(_, videoid, chat_id):
+    buttons = [
+        [           
+            InlineKeyboardButton(
+                text= "Menu",
+                callback_data=f"PanelMarkup None|{chat_id}",
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text= "Close",
+                callback_data="close"
+            )
+        ],
+    ]
+    return buttons
