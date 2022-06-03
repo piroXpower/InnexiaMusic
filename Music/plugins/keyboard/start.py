047e9ba4ac76cddbf12d4e2b39a5107f67f66a30
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-06-03 10:42:30 +0530

    Update start.py

--- Music/plugins/keyboard/start.py
+++ Music/plugins/keyboard/start.py
@@ -1,1 +1,13 @@
-
+start_markup(_, user_id) :
+keyboard = [
+             [
+                 InlineKeyboardButton("Help And Commands", callback_data="help"), 
+             ], 
+             [
+                 InlineKeyboardButton("About", callback_data="about"), 
+                 InlineKeyboardButton("Support", callback_data="support")
+             ], 
+             [
+                 InlineKeyboardButton("Add Me To Your Group", url=f"https://t.me/{bn}/startgroup=true),
+             ], 
+          ]
