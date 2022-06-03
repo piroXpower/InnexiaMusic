fe173d9383bbc8d53e4bec1ad6c812446d0e07d9
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-06-03 10:45:26 +0530

    Update __init__.py

--- Music/plugins/keyboard/__init__.py
+++ Music/plugins/keyboard/__init__.py
@@ -1,1 +1,6 @@
-from Music.plugins.keyboard import *
+from Music.plugins.keyboard.play import play_markup
+from Music.plugins.keyboard.start import start_markup
+
+
+playboard = play_markup
+startboard = start_markup 
