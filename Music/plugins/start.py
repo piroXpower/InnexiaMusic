88b9080b00681f798049f65aa7ae76cb8b7f7552
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-06-03 10:35:59 +0530

    Update start.py

--- Music/plugins/start.py
+++ Music/plugins/start.py
@@ -1,1 +1,9 @@
-
+@Client.on_message(other_filters2) 
+async def start(_, message, Message):
+        if START_IMG:
+         await message.reply_photo(
+                     Photo=START_IMG, 
+                     caption="", 
+                     reply_markup=InlineKeyboardButton(startboard), 
+                     disable_web_page_preview=True
+  ) 
